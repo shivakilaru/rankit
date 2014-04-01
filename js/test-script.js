7eff80791caeaf49f39a1c22637acf8934e8003a
Shiva Kilaru <skilaru14@gmail.com>
2014-04-01 16:45:32 -0400

    Getting rid of some old nonsense

--- js/test-script.js
+++ js/test-script.js
@@ -3,49 +3,5 @@ $(document).ready(function(){
 	//Fade In
 	$('body').hide();
 	$('body').fadeIn(1500);
-	
-	$('.logo').click(
-		function () {
-			window.location='index.html';
-	});
-	
-	$("#print").hover(
-	  function () {
-		$('#print-title').animate({
-			marginTop: '-50px'},
-			180);
-	  }, 
-	  function () {
-		$('#print-title').animate({
-			marginTop: '-150px'},
-			180);
-	  } 
-	);
-	
-	$("#screen").hover(
-	  function () {
-		$('#screen-title').animate({
-			marginTop: '-50px'},
-			180);
-	  }, 
-	  function () {
-		$('#screen-title').animate({
-			marginTop: '-150px'},
-			180);
-	  } 
-	);
-	
-	$("#photography").hover(
-	  function () {
-		$('#photo-title').animate({
-			marginTop: '-50px'},
-			180);
-	  }, 
-	  function () {
-		$('#photo-title').animate({
-			marginTop: '-150px'},
-			180);
-	  } 
-	);
 		
 });
