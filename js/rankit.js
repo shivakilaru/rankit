0a1309a56e54f84d57232a4e3f5f45ff5b9fb5c2
Shiva Kilaru <skilaru14@gmail.com>
2014-04-03 23:40:55 -0400

    Factors with weights

--- js/rankit.js
+++ js/rankit.js
@@ -5,17 +5,57 @@ $(document).ready(function(){
 	$('body').fadeIn(1500);
 
 
-	// === Adding Options and Factors, Screen 1 === //
+
+
+	/* ===============================
+	   ___ _       _           _     
+	  / _ \ | ___ | |__   __ _| |___ 
+	 / /_\/ |/ _ \| '_ \ / _` | / __|
+	/ /_\\| | (_) | |_) | (_| | \__ \
+	\____/|_|\___/|_.__/ \__,_|_|___/
+
+	================================= */
+
+	var optionList = new Array();
+	var factorList = new Array();
+	var combos = new Array();
+	var scores = {};
+
+
+
+
+
+
+
+	/* ===============================
+	 __                            _ 
+	/ _\ ___ _ __ ___  ___ _ __   / |
+	\ \ / __| '__/ _ \/ _ \ '_ \  | |
+	_\ \ (__| | |  __/  __/ | | | | |
+	\__/\___|_|  \___|\___|_| |_| |_|
+
+	================================= */
 	function addItem(type) {
 		var item = $('#'+type+'-box').val();
 		if (item === "") {
 		}
 		else if (item) {
-			$('#'+type+'-list').prepend(
-				"<li id='"+type+"-item'>" + item + 
-					"<a class='delete-"+type+"'></a>" + 
-				"</li>"
-			);
+
+			if (type == "option") {
+				$('#option-list').prepend(
+					"<li id='option-item'>" + item + 
+						"<a class='delete-option'></a>" + 
+					"</li>"
+				);
+			}
+			if (type == "factor") {
+				$('#factor-list').prepend(
+					"<li id='factor-item'>" + item + 
+						"<a class='delete-factor'></a>" + 
+						"<input type='range' min='0' max='50' step='1' value='25' class='factor-slider'/>" +
+					"</li>"
+				);
+			}
 
 			if ( $('#option-list').children().length > 1 ) {
 				$('#go-rank').addClass('clickable');
@@ -56,38 +96,86 @@ $(document).ready(function(){
 
 
 
-	// === Ranking, Screen 2 === //
+
+
+
+
+	/* ===============================
+	 __                            ____  
+	/ _\ ___ _ __ ___  ___ _ __   |___ \ 
+	\ \ / __| '__/ _ \/ _ \ '_ \    __) |
+	_\ \ (__| | |  __/  __/ | | |  / __/ 
+	\__/\___|_|  \___|\___|_| |_| |_____|
+
+	================================= */
+
 	$('#go-rank').click(function() {
 		beginRanking();
 	});
 
 	function beginRanking() {
-		var optionList = [];
-		var factorList = [];
-
 		for (i = 0; i < $('#option-list').children().length; i++) {
-			optionList.push($('#option-list').children()[i].textContent);
+			var optionText = $('#option-list').children().eq(i).text();
+			optionList.push(optionText);
 		}
 		for (i = 0; i < $('#factor-list').children().length; i++) {
-			factorList.push($('#factor-list').children()[i].textContent);
+			var factorText = $('#factor-list').children().eq(i).text();
+			var factorWeight = $('#factor-list').children().eq(i).children('.factor-slider').attr('value');
+			var factorInList = new Object();
+			factorInList["name"] = factorText;
+			factorInList["weight"] = factorWeight;
+			factorList.push(factorInList);
 		}
-
-		console.log("Options: " + optionList);
-		console.log("Factors: " + factorList);
 
 		if (optionList.length < 2) {
 			return;
 		}
 
-		else {
-			$('#new-rank-screen-1').fadeOut();
-			$('#new-rank-screen-2').append("<h2>Options</h2>"+optionList+"<br/><br/>");
-			$('#new-rank-screen-2').append("<h2>Factors</h2>"+factorList);
-			$('#new-rank-screen-2').fadeIn();
+		normalizeWeights();
+
+		console.log("Options: " + optionList);
+		console.log("Factors:");
+		for (f in factorList) {
+			console.log(factorList[f]['name'] + ": " + factorList[f]['weight']);
 		}
+
+		$('#new-rank-screen-1').hide();
+		$('#new-rank-screen-2').show();
 	}
 
 
+
+
+
+
+	/* ===============================
+	              _   _     
+	  /\/\   __ _| |_| |__  
+	 /    \ / _` | __| '_ \ 
+	/ /\/\ \ (_| | |_| | | |
+	\/    \/\__,_|\__|_| |_|
+	
+	================================= */
+
+	function factorial(n) {
+		if (n == 0) {
+			return 1;
+		}
+		else {
+			return n*factorial(n-1);	
+		}
+	}
+
+	function normalizeWeights() {
+		// Make total weight sum to 1
+		var sum = 0.0;
+		for (f in factorList) {
+			sum = parseFloat(sum) + parseFloat(factorList[f]['weight']);
+		}
+		for (f in factorList) {
+			factorList[f]['weight'] = 1.0 * factorList[f]['weight'] / sum;
+		}
+	}
 
 		
 });
