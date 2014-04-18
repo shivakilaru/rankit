9f7945eba6bbc6db35ad25878b08771f5bf38e35
Shiva Kilaru <skilaru14@gmail.com>
2014-04-18 03:01:50 -0400

    Browse boxes now link to respective rankits.

--- browse.php
+++ browse.php
@@ -10,20 +10,20 @@ require_once "db.php";
                                                                       
 	======================================================================= */
 
-function get_url_contents($url) {
-    $crl = curl_init();
+	function get_url_contents($url) {
+	    $crl = curl_init();
 
-    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-    curl_setopt($crl, CURLOPT_URL, $url);
-    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
+	    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+	    curl_setopt($crl, CURLOPT_URL, $url);
+	    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
 
-    $ret = curl_exec($crl);
-    curl_close($crl);
-    return $ret;
-}
+	    $ret = curl_exec($crl);
+	    curl_close($crl);
+	    return $ret;
+	}
 
-$url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=';
+	$url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=';
 
 
 
@@ -36,59 +36,62 @@ $url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=';
 	                                    
 	================================== */
 
-echo('
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+	echo('
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
-<html> 
+	<html> 
 
-	<head> 
+		<head> 
 
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-		<title>RankIt</title> 
+			<title>RankIt</title> 
 
-		<link rel="shortcut icon" type="image/png" href="img/icon.png" />
+			<link rel="shortcut icon" type="image/png" href="img/icon.png" />
 
-		<link rel="stylesheet" type="text/css" href="css/reset.css" />
-		<link rel="stylesheet" type="text/css" href="css/text.css" />
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-
-		<script src="js/jqBarGraph.1.1.min.js"></script>
-		<script src="js/masonry.pkgd.js"></script>
-		<script src="js/rankit.js"></script>
-
-
-	</head>
-	
-	<body>
-	
-		<div class="main-container">
+			<link rel="stylesheet" type="text/css" href="css/reset.css" />
+			<link rel="stylesheet" type="text/css" href="css/text.css" />
+			<link rel="stylesheet" type="text/css" href="css/style.css" />
 			
-			<div class="top-bar">
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 
-				<div class="logo">
-					<a href="index.php"><h1>Rankit</h1></a>
+			<script src="js/jqBarGraph.1.1.min.js"></script>
+			<script src="js/masonry.pkgd.js"></script>
+			<script src="js/rankit.js"></script>
+
+
+		</head>
+		
+		<body>
+		
+			<div class="main-container">
+				
+				<div class="top-bar">
+
+					<div class="logo">
+						<a href="index.php"><h1>Rankit</h1></a>
+					</div>
+
+					<ul class="navigation">
+						<li>Browse</li>
+						<li>My Rankits</li>
+						<li>New Rankit</li>
+					</ul>
+
 				</div>
 
-				<ul class="navigation">
-					<li>Browse</li>
-					<li>My Rankits</li>
-					<li>New Rankit</li>
-				</ul>
-
-			</div>
-
-			<hr/>
+				<hr/>
 
 
 
-			<div class="content-container">
-				<h2 style="font-weight:300;">BROWSE</h2>
+				<div class="content-container">
+					<h2 style="font-weight:300;">BROWSE</h2>
 
-				<div class="browseGrid">
-');	//End Echo - Header
+					<div class="browseGrid">
+	');	//End Echo - Header
+
+
+
 
 	/* ================================
 	    ____              __   _ __      
@@ -99,45 +102,47 @@ echo('
                                    
 	================================== */
 
-$sql = 'SELECT * FROM rankit';
-$stmt = $pdo->query($sql);
-$count = 0;
-while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
-{
-	//Retrieve User Information
-	$results = [];	//Clear previous image results
+	$sql = 'SELECT * FROM rankit';
+	$stmt = $pdo->query($sql);
+	$count = 0;
+	while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
+	{
+		//Retrieve User Information
+		$results = array();	//Clear previous image results
 
-	$userSQL = 'SELECT * FROM users WHERE id = '.$row['user_id'];
-	$userSTMT = $pdo->query($userSQL);
-	$userRow = $userSTMT->fetch(PDO::FETCH_ASSOC);
+		if ($row['user_id'] != NULL) {
+			$userSQL = "SELECT * FROM users WHERE id = ".$row['user_id'];
+			$userSTMT = $pdo->query($userSQL);
+			$userRow = $userSTMT->fetch(PDO::FETCH_ASSOC);
 
-	//Retrieve Picture for Rankit
+			//Retrieve Picture for Rankit
+			$imageQueryString = $row['title'];
+			$imageQueryString = str_replace(' ','%20',$imageQueryString);
+			$image_query = $url.$imageQueryString;
 
-	$imageQueryString = $row['name'];
-	$imageQueryString = str_replace(' ','%20',$imageQueryString);
-	$image_query = $url.$imageQueryString;
+			$json = get_url_contents($image_query);
+			$data = json_decode($json);
+			foreach ($data->responseData->results as $result) {
+		    $results[] = array('url' => $result->url, 'alt' => $result->title);
+			}
 
-	$json = get_url_contents($image_query);
-	$data = json_decode($json);
-	foreach ($data->responseData->results as $result) {
-    $results[] = array('url' => $result->url, 'alt' => $result->title);
+			echo('<a href="index.php?id='.$row['id'].'">');
+				echo('<div class ="browseBox">');
+					echo('<img class="browseBox-pic" src='.$results[3]['url'].'>');
+					echo('<h4 class="browseBox-title">'.$row['title'].'</h4>');
+					echo('<span class="browseBox-author">By: '.$userRow['first_name'].' '.$userRow['last_name'].'</span>');
+				echo('</div>');
+			echo('</a>');
+		}
 	}
 
-	echo('<div class ="browseBox">');
-	echo('<img class="browseBox-pic" src='.$results[0]['url'].'>');
-	echo('<h4 class="browseBox-title">'.$row['name'].'</h4>');
-	echo('<span class="browseBox-author">By: '.$userRow['first_name'].' '.
-		$userRow['last_name'].'</span>');
-	echo('</div>');
-}
-
-echo('					
+	echo('					
+					</div>	
 				</div>	
-			</div>	
-		</div>
-	</body>
-</html>
-');
+			</div>
+		</body>
+	</html>
+	');
 
 ?>
 	
