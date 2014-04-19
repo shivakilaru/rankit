1f19a6d63e94669920a49c2e56e485a11d9dba9f
Desmond Kolean-Burley <dkoleanb@umich.edu>
2014-04-19 19:39:20 -0400

    Implemented a header, edited the db file, deleted browse.html

--- browse.php
+++ browse.php
@@ -1,5 +1,5 @@
 <?php
-require_once "db.php";
+require_once "header.php";
 
 	/* =======================================================================
 	    ____  __  ______     ______                 __  _                 
@@ -25,72 +25,12 @@ require_once "db.php";
 
 	$url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=';
 
-
-
-	/* ================================
-	    __  __               __         
-	   / / / /__  ____ _____/ /__  _____
-	  / /_/ / _ \/ __ `/ __  / _ \/ ___/
-	 / __  /  __/ /_/ / /_/ /  __/ /    
-	/_/ /_/\___/\__,_/\__,_/\___/_/     
-	                                    
-	================================== */
-
 	echo('
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
-	<html> 
-
-		<head> 
-
-			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-			<title>RankIt</title> 
-
-			<link rel="shortcut icon" type="image/png" href="img/icon.png" />
-
-			<link rel="stylesheet" type="text/css" href="css/reset.css" />
-			<link rel="stylesheet" type="text/css" href="css/text.css" />
-			<link rel="stylesheet" type="text/css" href="css/style.css" />
-			
-			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-
-			<script src="js/jqBarGraph.1.1.min.js"></script>
-			<script src="js/masonry.pkgd.js"></script>
-			<script src="js/rankit.js"></script>
-
-
-		</head>
-		
-		<body>
-		
-			<div class="main-container">
-				
-				<div class="top-bar">
-
-					<div class="logo">
-						<a href="index.php"><h1>Rankit</h1></a>
-					</div>
-
-					<ul class="navigation">
-						<a href="browse.php" style="float:left;"><li>Browse</li></a>
-						<li>My Rankits</li>
-						<li>New Rankit</li>
-					</ul>
-
-				</div>
-
-				<hr/>
-
-
-
 				<div class="content-container">
 					<h2 style="font-weight:300;">BROWSE</h2>
 
 					<div class="browseGrid">
-	');	//End Echo - Header
-
-
+	');
 
 
 	/* ================================
--- index.php
+++ index.php
@@ -1,31 +1,6 @@
 <?php
-require_once "db.php";
+require_once "header.php";
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
-<html> 
-
-	<head> 
-
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-		<title>RankIt</title> 
-
-		<link rel="shortcut icon" type="image/png" href="img/icon.png" />
-
-		<link rel="stylesheet" type="text/css" href="css/reset.css" />
-		<link rel="stylesheet" type="text/css" href="css/text.css" />
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-
-		<script src="js/jqBarGraph.1.1.min.js"></script>
-		<script src="js/masonry.pkgd.js"></script>
-		<script src="js/rankit.js"></script>
-
-	</head>
-	
 	<body>
 
 		<?php
@@ -74,27 +49,6 @@ require_once "db.php";
 			});
 		</script>
 	
-		<div class="main-container">
-			
-			<div class="top-bar">
-
-				<div class="logo">
-					<a href="index.php"><h1>Rankit</h1></a>
-				</div>
-
-				<ul class="navigation">
-					<a href="browse.php" style="float:left;"><li>Browse</li></a>
-					<li>My Rankits</li>
-					<li>New Rankit</li>
-				</ul>
-
-				<div class="login-container">
-					<a id="login">Login</a>
-				</div>
-
-			</div>
-
-			<hr/>
 
 
 
